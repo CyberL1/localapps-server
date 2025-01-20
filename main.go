a9e0c55ecdf9f289798452a262de4a91606bf210
CyberL1 <cyber.hf18@gmail.com>
2025-01-20 18:08:27 +0100

    init

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/CyberL1/localapps/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ types/App.go
@@ -0,0 +1,14 @@
+package types
+
+type App struct {
+	Name  string `yaml:"name"`
+	Parts Part   `yaml:"parts"`
+}
+
+type Part struct {
+	// Name    string `yaml:"name"`
+	Src     string `yaml:"src"`
+	Path    string `yaml:"path,omitempty"`
+	Primary string `yaml:"primary"`
+	Run     string `yaml:"run"`
+}
