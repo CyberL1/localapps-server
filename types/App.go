a6e1d43c18f6fb50725834d2ab587f045ad3134e
CyberL1 <cyber.hf18@gmail.com>
2025-01-20 18:44:31 +0100

    fix(types): "parts" is a map

--- types/App.go
+++ types/App.go
@@ -2,11 +2,10 @@ package types
 
 type App struct {
 	Name  string `yaml:"name"`
-	Parts []Part `yaml:"parts"`
+	Parts map[string]Part `yaml:"parts"`
 }
 
 type Part struct {
-	// Name    string `yaml:"name"`
 	Src     string `yaml:"src"`
 	Path    string `yaml:"path,omitempty"`
 	Primary bool   `yaml:"primary,omitempty"`
