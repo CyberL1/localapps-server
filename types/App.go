809c92b9d91341b226d2ea972d465701b0132526
CyberL1 <cyber.hf18@gmail.com>
2025-01-20 18:27:10 +0100

    fix(types): "primary" is bool

--- types/App.go
+++ types/App.go
@@ -2,13 +2,13 @@ package types
 
 type App struct {
 	Name  string `yaml:"name"`
-	Parts []Part   `yaml:"parts"`
+	Parts []Part `yaml:"parts"`
 }
 
 type Part struct {
 	// Name    string `yaml:"name"`
 	Src     string `yaml:"src"`
 	Path    string `yaml:"path,omitempty"`
-	Primary string `yaml:"primary"`
+	Primary bool   `yaml:"primary,omitempty"`
 	Run     string `yaml:"run"`
 }
