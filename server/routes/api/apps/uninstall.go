70ded74c0fd139627cbb5f3e6148233b367b030c
CyberL1 <cyber.hf18@gmail.com>
2025-03-24 15:06:46 +0100

    feat(server): remove app container, volumes and images if there is one

--- server/routes/api/apps/uninstall.go
+++ server/routes/api/apps/uninstall.go
@@ -8,6 +8,12 @@ import (
 	"localapps/types"
 	"net/http"
 	"strings"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/api/types/volume"
+	dockerClient "github.com/docker/docker/client"
 )
 
 func uninstallApp(w http.ResponseWriter, r *http.Request) {
@@ -35,6 +41,45 @@ func uninstallApp(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, "App not installed", http.StatusInternalServerError)
 		return
+	}
+
+	cli, err := dockerClient.NewClientWithOpts(dockerClient.FromEnv)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to connect to docker engine: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	appContainers, _ := cli.ContainerList(context.Background(), container.ListOptions{Filters: filters.NewArgs(filters.Arg("name", "localapps-app-"+appId))})
+	if len(appContainers) > 0 {
+		for _, c := range appContainers {
+			err := cli.ContainerRemove(context.Background(), c.ID, container.RemoveOptions{Force: true})
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Error removing app conianer: %s", err), http.StatusInternalServerError)
+				return
+			}
+		}
+	}
+
+	storageVolumes, _ := cli.VolumeList(context.Background(), volume.ListOptions{Filters: filters.NewArgs(filters.Arg("name", "localapps-storage-"+appId))})
+	if len(storageVolumes.Volumes) > 0 {
+		for _, volume := range storageVolumes.Volumes {
+			err = cli.VolumeRemove(context.Background(), volume.Name, false)
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Error removing app storage: %s", err), http.StatusInternalServerError)
+				return
+			}
+		}
+	}
+
+	appImages, _ := cli.ImageList(context.Background(), image.ListOptions{Filters: filters.NewArgs(filters.Arg("reference", "localapps/apps/"+appId+"/*"))})
+	if len(appImages) > 0 {
+		for _, im := range appImages {
+			_, err = cli.ImageRemove(context.Background(), im.ID, image.RemoveOptions{})
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Error removing app image: %s", err), http.StatusInternalServerError)
+				return
+			}
+		}
 	}
 
 	err = client.DeleteApp(context.Background(), appId)
