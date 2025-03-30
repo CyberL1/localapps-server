35cd5fbf99bb194ff31d1b7a90be17c87d1c1b8a
CyberL1 <cyber.hf18@gmail.com>
2025-03-30 12:33:24 +0200

    fix(db_migrations): set icon default to empty string

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"localapps/cmd"
 	"localapps/constants"
 	"os"
-	"path/filepath"
 )
 
 func main() {
@@ -13,13 +12,6 @@ func main() {
 	if _, err := os.Stat(constants.LocalappsDir); os.IsNotExist(err) {
 		if err := os.Mkdir(constants.LocalappsDir, os.ModePerm); err != nil {
 			fmt.Println("Failed to create ~/.config/localapps directory:", err)
-			return
-		}
-	}
-
-	if _, err := os.Stat(filepath.Join(constants.LocalappsDir, "storage")); os.IsNotExist(err) {
-		if err := os.Mkdir(filepath.Join(constants.LocalappsDir, "storage"), os.ModePerm); err != nil {
-			fmt.Println("Failed to create ~/.config/localapps/storage directory:", err)
 			return
 		}
 	}
--- resources/db_migrations/20250328162630_app_icons.sql
+++ resources/db_migrations/20250328162630_app_icons.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-ALTER TABLE apps ADD COLUMN icon text NOT NULL;
+ALTER TABLE apps ADD COLUMN icon text NOT NULL DEFAULT '';
 
 -- +goose Down
 ALTER TABLE apps DROP COLUMN icon;
