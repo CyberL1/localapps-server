c813adf7e442be2280b000768fb51ff79723ac8e
CyberL1 <cyber.hf18@gmail.com>
2025-03-30 11:11:22 +0200

    fix: App uninstalling from home page

--- server/middlewares/ApiAuth.go
+++ server/middlewares/ApiAuth.go
@@ -11,22 +11,24 @@ func ApiAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("Authorization") == "" {
 			noAccessError := types.ApiError{
-				Code: "ACCESS_DENIED",
+				Code:    "ACCESS_DENIED",
 				Message: "Missing API Key",
 			}
 
 			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
 			json.NewEncoder(w).Encode(noAccessError)
 			return
 		}
 
 		if r.Header.Get("Authorization") != utils.CachedConfig.ApiKey {
 			noAccessError := types.ApiError{
-				Code: "ACCESS_DENIED",
+				Code:    "ACCESS_DENIED",
 				Message: "Invalid API Key",
 			}
 
 			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusForbidden)
 			json.NewEncoder(w).Encode(noAccessError)
 			return
 		}
