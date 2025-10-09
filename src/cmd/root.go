1bcfd03611d4e3e7214abc4d53f899af63d8ed00
CyberL1 <cyber.hf18@gmail.com>
2025-10-09 14:58:18 +0200

    chore(src): change command usage

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -8,7 +8,7 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "localapps",
+	Use:   "localapps-server",
 	Short: "Own your apps",
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
