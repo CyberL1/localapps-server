a29d1a0933f59da86b52abb56fb46f3edbc4d4e9
CyberL1 <cyber.hf18@gmail.com>
2025-10-11 22:52:42 +0200

    refactor(src): remove CLI-related things

--- src/types/Config.go
+++ src/types/Config.go
@@ -4,12 +4,3 @@ type ServerConfig struct {
 	AccessUrl string `default:"\"http://localhost:8080\""`
 	ApiKey    string `default:"\"\""`
 }
-
-type CliConfig struct {
-	Server CliConfigServer `json:"server"`
-}
-
-type CliConfigServer struct {
-	Url    string `json:"url"`
-	ApiKey string `json:"apiKey"`
-}
--- src/utils/Config.go
+++ src/utils/Config.go
@@ -4,11 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
-	"fmt"
-	"localapps-server/constants"
 	dbClient "localapps-server/db/client"
-	"os"
-	"path/filepath"
 
 	db "localapps-server/db/generated"
 	"localapps-server/types"
@@ -16,7 +12,6 @@ import (
 )
 
 var ServerConfig types.ServerConfig
-var CliConfig types.CliConfig
 
 func UpdateServerConfigCache() error {
 	err := validateServerConfig()
@@ -76,44 +71,5 @@ func validateServerConfig() error {
 
 		client.SetConfigKey(context.Background(), db.SetConfigKeyParams{Key: k, Value: sql.NullString{String: defaultValue, Valid: true}})
 	}
-	return nil
-}
-
-func UpdateCliConfigCache() error {
-	err := validateCliConfig()
-	if err != nil {
-		return err
-	}
-
-	configFile, err := os.Open(filepath.Join(constants.LocalappsDir, "cli-config.json"))
-	if err != nil {
-		return fmt.Errorf("cannot find cli config file: %s", err)
-	}
-
-	defer configFile.Close()
-	decoder := json.NewDecoder(configFile)
-	var config types.CliConfig
-	if err := decoder.Decode(&config); err != nil {
-		return fmt.Errorf("failed to decode cli config: %s", err)
-	}
-
-	CliConfig = config
-	return nil
-}
-
-func validateCliConfig() error {
-	_, err := os.Open(filepath.Join(constants.LocalappsDir, "cli-config.json"))
-	if err != nil {
-		os.WriteFile(filepath.Join(constants.LocalappsDir, "cli-config.json"), []byte("{\"server\":{\"url\":\"http://localhost:8080\"}}"), 0644)
-	}
-	return nil
-}
-
-func SaveCliConfig() error {
-	data, err := json.Marshal(CliConfig)
-	if err != nil {
-		return fmt.Errorf("failed to marshal CliConfig: %s", err)
-	}
-	os.WriteFile(filepath.Join(constants.LocalappsDir, "cli-config.json"), data, 0644)
 	return nil
 }
