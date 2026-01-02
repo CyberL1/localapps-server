e35531fff170783f15cbf654031c3265668504e7
CyberL1 <cyber.hf18@gmail.com>
2026-01-02 14:57:32 +0100

    refactor(web): remove trailing slash from apps request

--- src/web/src/routes/+page.ts
+++ src/web/src/routes/+page.ts
@@ -4,7 +4,7 @@ export const load: PageLoad = async ({ fetch }) => {
   const linkReq = await fetch("/api/link");
   const linkJson = await linkReq.json();
 
-  const appsReq = await fetch("/api/apps/", { headers: { Authorization: linkJson.ApiKey } });
+  const appsReq = await fetch("/api/apps", { headers: { Authorization: linkJson.ApiKey } });
   const appsJson = await appsReq.json();
 
   return {
